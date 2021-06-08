44e1b05053c1d81434cb4b1d8f6a3539d6b9fe5c
Sam Kearney <sam.kearney@etcconnect.com>
2021-06-08 13:24:48 -0500

    Finalize versioned build automation

--- tools/ci/deploy_to_artifactory.py
+++ tools/ci/deploy_to_artifactory.py
@@ -4,7 +4,10 @@ import os
 import shutil
 import zipfile
 
+from artifactory import ArtifactoryPath
+
 THIS_FILE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+ARTIFACTORY_BASE_URL = "https://etc.jfrog.io/artifactory/mdnswindows/"
 
 
 def stage_binaries():
@@ -33,13 +36,35 @@ def stage_binaries():
                     relative_path_from_base[len(os.path.join("build_x86", "install", "")) :],
                 )
 
-    shutil.copy2(os.path.join("tools", "install", "bin", "Release", "ETC_mDNSInstall.msm"),
-     os.path.join("deploy", "ETC_mDNSInstall.msm"))
+    shutil.copy2(
+        os.path.join("tools", "install", "bin", "Release", "ETC_mDNSInstall.msm"),
+        os.path.join("deploy", "ETC_mDNSInstall.msm"),
+    )
+
+
+def deploy_binaries(version: str):
+    """Deploys staged binaries to cloud Artifactory."""
+    path = ArtifactoryPath(
+        ARTIFACTORY_BASE_URL + f"/{version}", apikey=os.getenv("ETC_CLOUD_ARTIFACTORY_API_KEY")
+    )
+    path.mkdir()
+    path.deploy_file(os.path.join("deploy", "mDNSWindows_x64.zip"))
+    path.deploy_file(os.path.join("deploy", "mDNSWindows_x86.zip"))
+    path.deploy_file(os.path.join("deploy", "ETC_mDNSInstall.msm"))
+
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Deploy mDNSWindows artifacts to cloud Artifactory"
+    )
+    parser.add_argument("version", help="Artifact version being deployed")
+    args = parser.parse_args()
+
     # Make sure our cwd is the root of the repository
     os.chdir(os.path.abspath(os.path.join(THIS_FILE_DIRECTORY, "..", "..")))
+
     stage_binaries()
+    deploy_binaries(args.version)
 
 
 if __name__ == "__main__":

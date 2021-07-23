fda03c221f299016c8098f12e6f244f97ad965c2
Sam Kearney <sam.kearney@etcconnect.com>
2021-07-23 15:15:33 -0500

    Update CI to deploy to GitHub Releases

--- tools/ci/deploy_to_artifactory.py
+++ tools/ci/deploy_to_artifactory.py
@@ -2,12 +2,16 @@
 import argparse
 import os
 import shutil
+import sys
+import time
 import zipfile
 
-from artifactory import ArtifactoryPath
+from github import Github
 
 THIS_FILE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-ARTIFACTORY_BASE_URL = "https://etc.jfrog.io/artifactory/mdnswindows/"
+GH_REPO_IDENT = "ETCLabs/mDNSWindows"
+GH_USERNAME = "svc-etclabs"
+GH_API_TOKEN = os.getenv("SVC_ETCLABS_REPO_TOKEN")
 
 
 def stage_binaries():
@@ -16,7 +20,9 @@ def stage_binaries():
 
     os.makedirs("deploy", exist_ok=True)
 
-    with zipfile.ZipFile(os.path.join("deploy", "mDNSWindows_x64.zip"), "w") as archive:
+    with zipfile.ZipFile(
+        os.path.join("deploy", "mDNSWindows_x64.zip"), "w", zipfile.ZIP_DEFLATED
+    ) as archive:
         for (dirpath, dirnames, filenames) in os.walk(os.path.join("build_x64", "install")):
             for file in filenames:
                 relative_path_from_base = os.path.join(dirpath, file)
@@ -27,7 +33,9 @@ def stage_binaries():
                     relative_path_from_base[len(os.path.join("build_x64", "install", "")) :],
                 )
 
-    with zipfile.ZipFile(os.path.join("deploy", "mDNSWindows_x86.zip"), "w") as archive:
+    with zipfile.ZipFile(
+        os.path.join("deploy", "mDNSWindows_x86.zip"), "w", zipfile.ZIP_DEFLATED
+    ) as archive:
         for (dirpath, dirnames, filenames) in os.walk(os.path.join("build_x86", "install")):
             for file in filenames:
                 relative_path_from_base = os.path.join(dirpath, file)
@@ -43,14 +51,31 @@ def stage_binaries():
 
 
 def deploy_binaries(version: str):
-    """Deploys staged binaries to cloud Artifactory."""
-    path = ArtifactoryPath(
-        ARTIFACTORY_BASE_URL + f"/{version}", apikey=os.getenv("ETC_CLOUD_ARTIFACTORY_API_KEY")
+    """Deploys staged binaries to a new GitHub Release."""
+    g = Github(login_or_token=GH_USERNAME, password=GH_API_TOKEN)
+    repo = g.get_repo(GH_REPO_IDENT)
+
+    print(f"Waiting for the correct GitHub tag v{version} to become available...")
+
+    keep_trying = True
+    while keep_trying:
+        for tag in repo.get_tags():
+            if tag.name == f"v{version}":
+                keep_trying = False  # Tag now exists
+                break
+
+        if keep_trying:
+            time.sleep(5)
+
+    print(f"Tag v{version} available. Creating release...")
+    new_release = repo.create_git_release(
+        tag=f"v{version}",
+        name=f"mDNSWindows v{version}",
+        message=f"Automated release of mDNSWindows for v{version}",
     )
-    path.mkdir()
-    path.deploy_file(os.path.join("deploy", "mDNSWindows_x64.zip"))
-    path.deploy_file(os.path.join("deploy", "mDNSWindows_x86.zip"))
-    path.deploy_file(os.path.join("deploy", "ETC_mDNSInstall.msm"))
+    new_release.upload_asset(os.path.join("deploy", "mDNSWindows_x64.zip"))
+    new_release.upload_asset(os.path.join("deploy", "mDNSWindows_x86.zip"))
+    new_release.upload_asset(os.path.join("deploy", "ETC_mDNSInstall.msm"))
 
 
 def main():
